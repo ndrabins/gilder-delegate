99fc9ccf9cadfb00613639b5aa172bb7a8f22b15
Noah <noah.drabinsky1@gmail.com>
2022-04-02 09:12:01 -0600

    Update to rewired and update config

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,7 @@ import {
   WalletProvider,
 } from "@solana/wallet-adapter-react";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-import {
-  LedgerWalletAdapter,
-  PhantomWalletAdapter,
-  SlopeWalletAdapter,
-  SolflareWalletAdapter,
-  SolletExtensionWalletAdapter,
-  SolletWalletAdapter,
-  TorusWalletAdapter,
-} from "@solana/wallet-adapter-wallets";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
 import {
   WalletModalProvider,
   WalletDisconnectButton,
@@ -38,18 +30,7 @@ function App() {
   // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
   // Only the wallets you configure here will be compiled into your application, and only the dependencies
   // of wallets that your users connect to will be loaded.
-  const wallets = useMemo(
-    () => [
-      new PhantomWalletAdapter(),
-      new SlopeWalletAdapter(),
-      new SolflareWalletAdapter({ network }),
-      new TorusWalletAdapter(),
-      new LedgerWalletAdapter(),
-      new SolletWalletAdapter({ network }),
-      new SolletExtensionWalletAdapter({ network }),
-    ],
-    [network]
-  );
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], [network]);
 
   const theme = createTheme({
     palette: {
@@ -57,7 +38,9 @@ function App() {
     },
   });
 
-  return (
+  //github.com/solana-labs/wallet-adapter/tree/master/packages/starter/create-react-app-starter
+
+  https: return (
     <ThemeProvider theme={theme}>
       <ConnectionProvider endpoint={endpoint}>
         <WalletProvider wallets={wallets}>
--- src/Delegate.tsx
+++ src/Delegate.tsx
@@ -1,7 +1,7 @@
 import { Button, TextField, Box } from "@mui/material";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styled from "@emotion/styled";
-import { withSetGovernanceDelegate } from "@solana/spl-governance";
+import { withSetGovernanceDelegate, getRealms } from "@solana/spl-governance";
 import {
   PublicKey,
   ConfirmedSignatureInfo,
@@ -12,6 +12,22 @@ import {
   Keypair,
 } from "@solana/web3.js";
 
+// if (rawRealm.account.communityMint) {
+//   communityMintPromise = connection.getParsedAccountInfo(
+//     new PublicKey(rawRealm.account.communityMint)
+//   );
+// }
+// if (rawRealm.account.config.councilMint) {
+//   councilMintPromise = connection.getParsedAccountInfo(
+//     new PublicKey(rawRealm.account.config.councilMint)
+//   );
+// }
+
+export const REALM_GOVERNANCE_PROGRAM_ID =
+  "GovER5Lthms3bLBqWub97yVrMmEogzX7xNjdXpPPCVZw";
+
+export const REALM_GOVERNANCE_PKEY = new PublicKey(REALM_GOVERNANCE_PROGRAM_ID);
+
 const RPC_CONNECTION = "https://ssc-dao.genesysgo.net/";
 let connection = new Connection(RPC_CONNECTION, "confirmed");
 
@@ -20,6 +36,16 @@ export const Delegate = () => {
     "5YWDXAX1xygHp4t7wjmPzzfWuybEuKWmd3ojUBnJtkxq"
   );
 
+  useEffect(() => {
+    // fetch realms
+    // const realms = fetchRealms();
+    // console.log("realms", realms);
+  }, []);
+
+  // const fetchRealms = async () => {
+  //   return await getRealms(connection, REALM_GOVERNANCE_PKEY);
+  // };
+
   const handleDelegate = () => {
     const signers: Keypair[] = [];
     const instructions: TransactionInstruction[] = [];
@@ -27,8 +53,9 @@ export const Delegate = () => {
     // 1. Fetch all realms
     // 2. user selects a realm,
     // get program version, programID, public key, and token mints of realm
+    // 3. Check if user has that token deposited (disable button if not deposited)
     // 3. User selects token they want to delegate (council/community)
-    // 4.
+    // 4. Onclick delegate - fire transaction
 
     // withSetGovernanceDelegate(
     //   // instructions,
@@ -44,6 +71,7 @@ export const Delegate = () => {
     console.log("publicKey", publicKey);
   };
 
+  // TODO: make a form stepper for each action a user needs to do with delegate action at end
   return (
     <Box
       sx={{ justifyContent: "center", alignItems: "center", display: "flex" }}
